d3adabb4f5e8027929a457c26be8dcae05471a6b
Szőke Mihály <szoke.mihaly97@gmail.com>
2025-10-19 18:03:26 +0200

    I made the Ui

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-const x = "rock";
-const y = "scissor";
-const z = "paper";
-
 let humanScore = 0;
 let computerScore = 0;
 
+let rock = document.getElementById("rock");
+let scissor = document.getElementById("scissor");
+let paper = document.getElementById("paper");
 
+let roundDisplay = document.getElementById("round");
+let statusDisplay = document.getElementById("status");
+let scoreDisplay = document.getElementById("score");
+let computerChoiceDisplay = document.getElementById("computerChoice");
 
+rock.addEventListener("click",() => playRound ("rock"));
+scissor.addEventListener("click", () => playRound ("scissor"));
+paper.addEventListener("click", () => playRound ("paper"));
+
+let round = 1;
 
 function game(humanChoice, computerChoice) {
-      
-        if (humanChoice === computerChoice) {
-        console.log("The result: It's a draw!");
-    }
-
-    else if (
-        (humanChoice === x && computerChoice === y) ||
-        (humanChoice === y && computerChoice === z) ||
-        (humanChoice === z && computerChoice === x)) {
+    if (humanChoice === computerChoice) {
+        statusDisplay.textContent = "Draw!";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissor") ||
+        (humanChoice === "scissor" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock"))
+        {
             humanScore++;
-         console.log(`The result: You won ${humanChoice} beats ${computerChoice}! `);
-        }
-    else {
-        computerScore++;
-        console.log(`The result: You lost ${computerChoice} beats ${humanChoice}! `);
+            statusDisplay.textContent = `You Win! ${humanChoice} beats ${computerChoice}`;
+        } else {
+            computerScore ++;
+            statusDisplay.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+} 
+const choices = ["rock", "paper", "scissor"];
+
+
+function playRound(humanChoice) {
+      
+     if (humanScore >= 5 || computerScore >= 5) return
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    game(humanChoice, computerChoice);
+    computerChoiceDisplay.textContent = `Computer chose: ${computerChoice}`;
+    scoreDisplay.textContent = `Player: ${humanScore} | Computer ${computerScore}`;
+    roundDisplay.textContent = `Round: ${round++}`;
+
+    if (humanScore === 5) {
+        statusDisplay.textContent = "Congratulation, you won!";
+    } else if (computerScore === 5) {
+        statusDisplay.textContent = "You lost, the computer won!";
     }
     
 }
-
-const choices = [x, y, z];
-let round = 1;
-while (humanScore <5 && computerScore <5) {
-    let human = prompt("Rock, Scissor, Paper?" + "").toLowerCase();
-    let computer = choices[Math.floor(Math.random() * choices.length)];
-    game(human, computer);
-    console.log("Computer answer: " + computer);
-    console.log("Player score: " + humanScore + " " + "Computer score: " + computerScore);
-    console.log("Round: " + round++ );
-}
-
-    if (humanScore===5) {
-        console.log("You won!");
- }  else {
-        console.log("Computer won!")
-    
- }
